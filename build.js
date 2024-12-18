0ce7eda43f184bdc55b35fec91bc3bc7d537522f
Chris Alwin <chris.alwin@youngshand.com>
2024-12-19 11:32:15 +1300

    Add build to minify the output

---
+++ build.js
@@ -0,0 +1,17 @@
+import esbuild from "esbuild";
+import glob from "fast-glob";
+
+(async () => {
+  const entryPoints = await glob("dist/**/*.js"); // Match only .js files
+
+  esbuild.build({
+    entryPoints,
+    outdir: "dist",
+    bundle: false,
+    minify: true,
+    sourcemap: true,
+    platform: "node",
+    target: "es2020",
+    allowOverwrite: true
+  }).catch(() => process.exit(1));
+})();
